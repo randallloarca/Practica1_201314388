bfbce7ba3600c38b051b8a92625f99b98c1b47d2
MariaJo <MariaJo@DESKTOP-84601S1>
2017-08-23 12:10:19 -0600

    commit final

--- src/practica1_buscaminas/menuprincipal.java
+++ src/practica1_buscaminas/menuprincipal.java
@@ -37,16 +37,18 @@ public class menuprincipal {
         System.out.println("3. Experto");
         System.out.println("4. Salir");
         System.out.println("");
-        System.out.println("");
         System.out.println("Ingrese Opción");
         num = sc.nextInt();
         System.out.println("----------------------------------------------------");
         switch(num){
             case 1:
+                ModoDeJuego facil = new ModoDeJuego(1);
             break;
             case 2:
+                ModoDeJuego intermedio = new ModoDeJuego(2);
             break;
             case 3:
+                ModoDeJuego difil = new ModoDeJuego(3);
             break;
             case 4:
             default:
